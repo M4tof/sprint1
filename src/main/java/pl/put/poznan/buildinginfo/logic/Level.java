1da54496aa1f30ec60aeaf604fdef3ed36420d99
inzoprogramowania <fsfsretryyryf@gmail.com>
2024-12-15 18:53:17 +0100

    Update Level.java
    Dodanie komentarzy

--- src/main/java/pl/put/poznan/buildinginfo/logic/Level.java
+++ src/main/java/pl/put/poznan/buildinginfo/logic/Level.java
@@ -4,13 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Klasa reprezentująca piętro w budynku.
+ * <p>
+ * Celem klasy {@code Level} jest zarządzanie pomieszczeniami znajdującymi się na jednym piętrze budynku.
+ * Piętro stanowi część większej struktury, czyli całego budynku, a klasa ta umożliwia przechowywanie i manipulowanie
+ * danymi o pomieszczeniach w obrębie tego piętra.
+ * </p>
+ * <p>
+ * Klasa oferuje metody umożliwiające obliczenie istotnych informacji o pomieszczeniach, takich jak powierzchnia,
+ * kubatura, zapotrzebowanie grzewcze czy średnie oświetlenie dla całego piętra. Dzięki tym metodom, możliwe jest
+ * uzyskanie przeglądu przestrzeni piętra oraz obliczenie parametrów energetycznych.
+ * </p>
+ * <p>
+ * Klasa zapewnia dostęp do tych danych poprzez odpowiednie metody dostępu (gettery/settery) oraz umożliwia ich modyfikację
+ * w razie potrzeby. Dodatkowo, zapewnia operacje agregujące, takie jak dodawanie, usuwanie i wyszukiwanie pomieszczeń.
+ * </p>
+ */
 public class Level {
-    private String id;
-    private String name; // Optional
-    private List<Room> rooms;
+    private String id; // Unikalny identyfikator piętra
+    private String name; // Optional nazwa piętra
+    private List<Room> rooms; // Lista pomieszczeń na piętrze
 
     // Constructor
+    /**
+     * Konstruktor domyślny bez parametrów.
+     */
     public Level(){}
+
+    /**
+     * Tworzy piętro o podanym identyfikatorze oraz nazwie.
+     *
+     * @param id unikalny identifikator piętra
+     * @param name opcjonalna nazwa piętra
+     */
     public Level(String id, String name) {
         this.id = id;
         this.name = name;
@@ -18,39 +45,86 @@ public class Level {
     }
 
     // Methods to manage rooms
+    /**
+     * Dodaje pomieszczenie do piętra poprzez dodanie go do listy pomieszczeń piętra.
+     *
+     * @param room pomieszczenie do dodania
+     */
     public void addRoom(Room room) {
         this.rooms.add(room);
     }
 
+    /**
+     * Usuwa pomieszczenie z piętra poprzez usunięcie go z listy pomieszczeń piętra.
+     *
+     * @param room pomieszczenie do usunięcia
+     */
     public void removeRoom(Room room) {
         this.rooms.remove(room);
     }
 
     // Getters and setters
+    /**
+     * Zwraca identyfikator piętra.
+     *
+     * @return identyfikator piętra
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Ustawia identyfikator piętra.
+     *
+     * @param id identyfikator piętra do ustawienia
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Zwraca nazwę piętra.
+     *
+     * @return nazwa piętra
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Ustawia nazwę piętra.
+     *
+     * @param name nazwa piętra
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Zwraca pomieszczenia na piętrze.
+     *
+     * @return pomieszczenia na piętrze
+     */
     public List<Room> getRooms() {
         return rooms;
     }
 
+    /**
+     * Zmienia pomieszczenia na piętrze na podanae.
+     *
+     * @param rooms pomieszczenia do ustawienia
+     */
     public void setRooms(List<Room> rooms) {
         this.rooms = rooms;
     }
 
+    /**
+     * Wyszukuje pomieszczenia o szukanym identyfikatorze.
+     *
+     * @param Id identyfikator pomieszczenia do wyszukania
+     *
+     * @return pomieszczenie o szukanym identyfikatorze lub null jeśli nie istnieje
+     */
     public Room getRoomById(String Id){
         for (Room i: rooms){
             if ( Objects.equals(Id,i.getId()) ){
@@ -60,6 +134,11 @@ public class Level {
         return null;
     }
 
+    /**
+     * Zwraca powierzchnię piętra.
+     *
+     * @return powierzchnia piętra
+     */
     public float getLevelArea(){
         float sum = 0;
         for(Room room : this.getRooms()){
@@ -68,6 +147,11 @@ public class Level {
         return sum;
     }
 
+    /**
+     * Zwraca kubaturę pomieszczenia.
+     *
+     * @return objętość pomieszczenia
+     */
     public float getLevelCube(){
         float sum = 0;
         for(Room room : this.getRooms()){
@@ -76,6 +160,11 @@ public class Level {
         return sum;
     }
 
+    /**
+     * Oblicza średnii poziom oświetlenia na piętrze.
+     *
+     * @return średnie oświetlenie na piętrze lub 0 jeśli piętro jest puste
+     */
     public float getLevelLight(){
         float lightsum = 0, areasum = 0;
         for(Room room : this.getRooms()){
@@ -88,6 +177,11 @@ public class Level {
         return 0;
     }
 
+    /**
+     * Zwraca średnie zapotrzebowanie grzewcze.
+     *
+     * @return średnie zapotrzebowanie grzewcze
+     */
     public float getLevelHeating(){
         float heatingsum = 0, cubesum = 0;
         for(Room room : this.getRooms()){
@@ -100,6 +194,11 @@ public class Level {
         return 0;
     }
 
+    /**
+     * Zwraca tekstową reprezentację piętra.
+     *
+     * @return tekstowa reprezentacja piętra
+     */
     @Override
     public String toString() {
         return "Level{" +
